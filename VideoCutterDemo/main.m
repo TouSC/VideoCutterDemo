76900730bc7a2f0699b6b54c2370ad6b30011e7d
fanmaoyu0871 <frank0871@163.com>
2016-02-26 14:16:26 +0800

    Initial Commit

---
+++ VideoCutterDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VideoCutterDemo
+//
+//  Created by 范茂羽 on 16/2/26.
+//  Copyright © 2016年 范茂羽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
